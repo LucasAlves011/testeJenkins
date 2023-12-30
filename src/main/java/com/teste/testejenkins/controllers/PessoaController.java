a11ef1c5aa2d9f67e075ab3f6199d24837e17e02
Lucas Alves <lucas.nascimento.al16@gmail.com>
2023-12-30 14:33:43 -0300

    first commit

---
+++ src/main/java/com/teste/testejenkins/controllers/PessoaController.java
@@ -0,0 +1,15 @@
+package com.teste.testejenkins.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PessoaController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World");
+    }
+
+}
